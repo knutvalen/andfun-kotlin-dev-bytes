c7c8153dc5b508b6101ebddcf3a4c9549a0035c1
Knut Valen <knut.valen@icloud.com>
2020-12-03 12:51:50 +0100

    kjhugyfd

--- app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
+++ app/src/main/java/com/example/android/devbyteviewer/network/DataTransferObjects.kt
@@ -17,6 +17,7 @@
 
 package com.example.android.devbyteviewer.network
 
+import com.example.android.devbyteviewer.database.DatabaseVideo
 import com.example.android.devbyteviewer.domain.Video
 import com.squareup.moshi.JsonClass
 
@@ -59,10 +60,23 @@ data class NetworkVideo(
 fun NetworkVideoContainer.asDomainModel(): List<Video> {
     return videos.map {
         Video(
-                title = it.title,
-                description = it.description,
-                url = it.url,
-                updated = it.updated,
-                thumbnail = it.thumbnail)
+            title = it.title,
+            description = it.description,
+            url = it.url,
+            updated = it.updated,
+            thumbnail = it.thumbnail
+        )
     }
+}
+
+fun NetworkVideoContainer.asDatabaseModel(): Array<DatabaseVideo> {
+    return videos.map {
+        DatabaseVideo(
+            url = it.url,
+            updated = it.updated,
+            title = it.title,
+            description = it.description,
+            thumbnail = it.thumbnail
+        )
+    }.toTypedArray()
 }
